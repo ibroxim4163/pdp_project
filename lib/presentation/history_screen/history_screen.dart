aecf042ddc745599f18551415fb7152eef646ede
ibroxim4163 <ibrohimjoxongirov2@gmail.com>
2023-10-20 21:56:13 +0500

    bottom app bar is ready

--- lib/presentation/history_screen/history_screen.dart
+++ lib/presentation/history_screen/history_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pdp_project/presentation/widgets/sections.dart';
+
 import '../../data/constants/app_colors.dart';
+
 
 class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
@@ -10,88 +13,82 @@ class HistoryScreen extends StatefulWidget {
 
 class _HistoryScreenState extends State<HistoryScreen> {
   ValueNotifier<bool> isSelected = ValueNotifier(true);
- 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ListView(
-
-
-          children:const [
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     TextButton(
-            //       onPressed: () {
-            //         setState(() {
-            //           isSelected.value = true;
-            //         });
-            //       },
-            //       child: Text(
-            //         "Input",
-            //         style: TextStyle(
-            //           fontSize: 17,
-            //           color: isSelected.value
-            //               ? AppColors.mainColor
-            //               : AppColors.greyColor,
-            //         ),
-            //       ),
-            //     ),
-            //     const SizedBox(width: 20),
-            //     TextButton(
-            //       onPressed: () {
-            //         setState(() {
-            //           isSelected.value = false;
-            //         });
-            //       },
-            //       child: Text(
-            //         "Output",
-            //         style: TextStyle(
-            //           fontSize: 17,
-            //           color: !isSelected.value
-            //               ? AppColors.mainColor
-            //               : AppColors.greyColor,
-            //         ),
-            //       ),
-            //     ),
-            //   ],
-            // ),
-            // Padding(
-            //   padding: const EdgeInsets.symmetric(horizontal: 20),
-            //   child: SizedBox(
-            //     height: 5,
-            //     width: double.infinity,
-            //     child: Row(
-            //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //       children: [
-            //         SizedBox(
-            //           height: 3,
-            //           width: MediaQuery.sizeOf(context).width * .5 - 20,
-            //           child: ColoredBox(
-            //             color: isSelected
-            //                 ? AppColors.mainColor
-            //                 : AppColors.greyColor,
-            //           ),
-            //         ),
-            //         SizedBox(
-            //           height: 3,
-            //           width: MediaQuery.sizeOf(context).width * .5 - 20,
-            //           child: ColoredBox(
-            //             color: !isSelected
-            //                 ? AppColors.mainColor
-            //                 : AppColors.greyColor,
-            //           ),
-            //         ),
-            //       ],
-            //     ),
-            //   ),
-            // ),
-          ],
+      appBar: PreferredSize(
+        preferredSize: const Size(double.infinity, 70),
+        child: SafeArea(
+          child: HistorySections(
+            isSelected: isSelected,
+            firstText: "Input",
+            secondText: "Output",
+          ),
         ),
+      ),
+      body: ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        separatorBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: SizedBox(
+              width: double.infinity,
+              height: 1.5,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: const Color(0xFF20BC82).withOpacity(.5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xFF05AF70).withOpacity(0.5),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    "Tualet bumaga elma soft touch (8 sht)",
+                    maxLines: 2,
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black,
+                          fontFamily: "Inter-Regular",
+                        ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 8),
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Text(
+                        "22.01.23",
+                        style:
+                            Theme.of(context).textTheme.titleMedium!.copyWith(
+                                  fontWeight: FontWeight.w700,
+                                  color: AppColors.mainColor,
+                                  fontFamily: "Inter-Regular",
+                                ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
 }
-
-
